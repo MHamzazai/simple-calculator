d3a6f70b25da32b610db35120f32a1acf5d46616
MHamzazai <farhatifraqfarhatifraq@gmail.com>
2024-04-08 19:44:45 +0500

    customize answer

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 //SHABANG(#! /usr/bin/env node);
 import inquirer from "inquirer";
 console.log("Welcome To The Cli Based Simple Calculator!.");
-;
 const answer = await inquirer.prompt([
     { message: "Enter your first number", type: "number", name: "firstNumber" },
     { message: "Enter your second number", type: "number", name: "secondNumber" },
@@ -13,20 +12,19 @@ const answer = await inquirer.prompt([
         choices: ["Addition", "Subtraction", "Multiplication", "Division"],
     },
 ]);
-console.log("Your Answer Showed Below!.");
 // CONDITIONAL IF STATMENT 
 if (answer.operator === "Addition") {
-    console.log(answer.firstNumber + answer.secondNumber);
+    console.log("Ans =", answer.firstNumber + answer.secondNumber);
 }
 //ELSE IF STATEMENT
 else if (answer.operator === "Subtraction") {
-    console.log(answer.firstNumber - answer.secondNumber);
+    console.log("Ans =", answer.firstNumber - answer.secondNumber);
 }
 else if (answer.operator === "Multiplication") {
-    console.log(answer.firstNumber * answer.secondNumber);
+    console.log("Ans =", answer.firstNumber * answer.secondNumber);
 }
 else if (answer.operator === "Division") {
-    console.log(answer.firstNumber / answer.secondNumber);
+    console.log("Ans =", answer.firstNumber / answer.secondNumber);
 }
 else {
     console.log("please selesct a valid operator");
